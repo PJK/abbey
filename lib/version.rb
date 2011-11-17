8c6b0094346bee7560bccbf999e4e0f662674d15
Pavel Kalvoda <me@pavelkalvoda.com>
2011-11-17 13:40:24 +0100

    bump version

--- lib/version.rb
+++ lib/version.rb
@@ -2,8 +2,8 @@ module Abbey
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 0
-    BUILD = 'beta3'
+    PATCH = 1
+    BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
